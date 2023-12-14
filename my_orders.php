f29b2f4758add4511f3fa5705db0df58f027735b
Alex Mihalașcu <alexmihalascu@gmail.com>
2023-12-14 11:09:11 +0200

    Add order details and address to my_orders.php

--- my_orders.php
+++ my_orders.php
@@ -13,13 +13,34 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 
-// Interogarea pentru a obține comenzile utilizatorului
-$sql = "SELECT order_id, order_date, order_status FROM orders WHERE user_id = ?";
+$sql = "SELECT o.order_id, o.order_date, o.order_status, da.address, 
+        SUM(p.price * od.quantity) AS total
+        FROM orders o
+        JOIN order_details od ON o.order_id = od.order_id
+        JOIN products p ON od.product_id = p.product_id
+        LEFT JOIN delivery_addresses da ON o.user_id = da.user_id
+        WHERE o.user_id = ?
+        GROUP BY o.order_id";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 $orders = $result->fetch_all(MYSQLI_ASSOC);
+
+$orderDetails = [];
+foreach ($orders as $order) {
+    $sql = "SELECT p.name, od.quantity, p.price FROM order_details od JOIN products p ON od.product_id = p.product_id WHERE od.order_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $order['order_id']);
+    $stmt->execute();
+    $details = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $orderDetails[$order['order_id']] = $details;
+}
+
+foreach ($orders as $key => $order) {
+    $formattedDate = date('d.m.Y H:i:s', strtotime($order['order_date']));
+    $orders[$key]['formatted_order_date'] = $formattedDate;
+}
 ?>
 
 <!DOCTYPE html>
@@ -34,19 +55,29 @@ $orders = $result->fetch_all(MYSQLI_ASSOC);
 <body>
     <div class="container mt-4">
         <h2>Comenzile Mele</h2>
-        <table class="table table-striped">
-            <thead>
+        <table class="table table-bordered table-hover">
+            <thead class="table-light">
                 <tr>
                     <th>ID Comandă</th>
                     <th>Data Comenzii</th>
-                    <th>Starea Comenzii</th>
+                    <th>Detalii Comandă</th>
+                    <th>Adresa de Livrare</th>
+                    <th>Suma Totală</th>
+                    <th>Stare</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($orders as $order): ?>
                     <tr>
                         <td><?= htmlspecialchars($order['order_id']) ?></td>
-                        <td><?= htmlspecialchars($order['order_date']) ?></td>
+                        <td><?= $order['formatted_order_date'] ?></td>
+                        <td>
+                            <?php foreach ($orderDetails[$order['order_id']] as $detail): ?>
+                                <p><?= htmlspecialchars($detail['name']) ?> - <?= $detail['quantity'] ?> buc. (<?= number_format($detail['price'], 2) ?> Lei/buc)</p>
+                            <?php endforeach; ?>
+                        </td>
+                        <td><?= htmlspecialchars($order['address']) ?></td>
+                        <td><?= number_format($order['total'], 2) ?> Lei</td>
                         <td><?= htmlspecialchars($order['order_status']) ?></td>
                     </tr>
                 <?php endforeach; ?>
